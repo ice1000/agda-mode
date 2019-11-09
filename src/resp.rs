10943f39fc80ad8daf4232616451a0aa7a0d24ef
ice1000 <ice1000kotlin@foxmail.com>
2019-11-08 19:20:58 -0500

    [ resp ] More

--- src/resp.rs
+++ src/resp.rs
@@ -27,3 +27,67 @@ pub struct Status {
     show_implicit_arguments: bool,
     checked: bool,
 }
+
+#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub enum DisplayInfo {
+    // TODO
+}
+
+pub type InteractionPoint = u32;
+
+/// TODO: This enum is incomplete, contribution is welcomed.
+#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Debug, Eq, PartialEq)]
+pub enum Resp {
+    HighlightingInfo {
+        kind: KindTag,
+        filepath: String,
+        direct: bool,
+    },
+    Status {
+        kind: KindTag,
+        status: Status,
+    },
+    JumpToError {
+        kind: KindTag,
+        // TODO
+    },
+    InteractionPoints {
+        kind: KindTag,
+        interaction_points: Vec<InteractionPoint>,
+    },
+    GiveAction {
+        kind: KindTag,
+        give_result: bool,
+        interaction_point: InteractionPoint,
+    },
+    MakeCase {
+        kind: KindTag,
+        // TODO
+    },
+    SolveAll {
+        kind: KindTag,
+        // TODO
+    },
+    DisplayInfo {
+        kind: KindTag,
+        info: DisplayInfo,
+    },
+    RunningInfo {
+        kind: KindTag,
+        // TODO
+    },
+    ClearRunningInfo {
+        kind: KindTag,
+        // TODO
+    },
+    ClearHighlighting {
+        kind: KindTag,
+        // TODO
+    },
+    DoneAborting {
+        kind: KindTag,
+        // TODO
+    },
+}
