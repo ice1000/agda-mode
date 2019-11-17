ed4cd3d1b17b7a0743a62bfa0a15fd56acec5196
ice1000 <ice1000kotlin@foxmail.com>
2019-11-17 17:17:28 -0500

    [ oc.rs ] Start working on pretty printing oc

--- src/resp/oc.rs
+++ src/resp/oc.rs
@@ -1,5 +1,6 @@
 use crate::base::{Comparison, InteractionPoint, Polarity};
 use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Error, Formatter};
 
 #[serde(rename_all = "camelCase")]
 #[derive(Serialize, Deserialize, Clone, Default, Debug, Eq, PartialEq)]
@@ -62,7 +63,7 @@ pub enum OutputConstraint<Obj> {
     CmpTeles(CmpSomething<Obj>),
     CmpSorts(CmpSomething<Obj>),
     Guard {
-        constraint_objs: Box<OutputConstraint<Obj>>,
+        constraint: Box<OutputConstraint<Obj>>,
         problem: String,
     },
     Assign {
@@ -106,3 +107,74 @@ pub enum OutputConstraint<Obj> {
 
 pub type VisibleGoal = OutputConstraint<InteractionPoint>;
 pub type InvisibleGoal = OutputConstraint<String>;
+
+pub trait CollectObjs<Obj> {
+    fn collect_objs(&self, collect: impl FnMut(&Obj) + Copy);
+}
+
+impl<Obj> CollectObjs<Obj> for JustSomething<Obj> {
+    fn collect_objs(&self, collect: impl FnMut(&Obj) + Copy) {
+        collect(&self.constraint_obj)
+    }
+}
+
+impl<Obj> CollectObjs<Obj> for Vec<Obj> {
+    fn collect_objs(&self, collect: impl FnMut(&Obj) + Copy) {
+        self.iter().for_each(collect)
+    }
+}
+
+impl<Obj> CollectObjs<Obj> for (Obj, Obj) {
+    fn collect_objs(&self, collect: impl FnMut<&Obj> + Copy) {
+        let (a, b) = self;
+        collect(a);
+        collect(b);
+    }
+}
+
+impl<Obj> CollectObjs<Obj> for CmpSomething<Obj> {
+    fn collect_objs(&self, collect: impl FnMut(&Obj) + Copy) {
+        self.constraint_objs.collect_objs(collect);
+    }
+}
+
+impl<Obj> CollectObjs<Obj> for OutputConstraint<Obj> {
+    fn collect_objs(&self, mut collect: impl FnMut(&Obj) + Copy) {
+        use OutputConstraint::*;
+        match self {
+            OfType { constraint_obj, .. } => collect(constraint_obj),
+            CmpInType {
+                constraint_objs, ..
+            } => constraint_objs.collect_objs(collect),
+            CmpElim {
+                constraint_objs: (xs, ys),
+                ..
+            } => {
+                xs.collect_objs(collect);
+                ys.collect_objs(collect);
+            }
+            JustType(a) => a.collect_objs(collect),
+            JustSort(a) => a.collect_objs(collect),
+            CmpTypes(c) => c.collect_objs(collect),
+            CmpLevels(c) => c.collect_objs(collect),
+            CmpTeles(c) => c.collect_objs(collect),
+            CmpSorts(c) => c.collect_objs(collect),
+            Guard { constraint, .. } => constraint.collect_objs(collect),
+            Assign { constraint_obj, .. } => collect(constraint_obj),
+            TypedAssign { constraint_obj, .. } => collect(constraint_obj),
+            PostponedCheckArgs(o) => unimplemented!(),
+            IsEmptyType { .. } => {}
+            SizeLtSat { .. } => {}
+            FindInstanceOF { constraint_obj, .. } => collect(constraint_obj),
+            PTSInstance { constraint_objs } => constraint_objs.collect_objs(collect),
+            PostponedCheckFunDef { .. } => {}
+        }
+    }
+}
+
+impl<Obj: Display> Display for OutputConstraint<Obj> {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        use OutputConstraint::*;
+        unimplemented!()
+    }
+}
