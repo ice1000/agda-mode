97f9503eab0f114846c8b70200e031ac809ddff8
ice1000 <ice1000kotlin@foxmail.com>
2019-11-17 17:28:41 -0500

    [ oc.rs ] Some tool functions

--- src/resp/oc.rs
+++ src/resp/oc.rs
@@ -33,15 +33,18 @@ pub struct CmpSomething<Obj> {
     pub comparison: Comparison,
 }
 
+#[serde(rename_all = "camelCase")]
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
+pub struct OfType<Obj> {
+    constraint_obj: Obj,
+    #[serde(rename = "type")]
+    of_type: String,
+}
+
 #[serde(tag = "kind")]
 #[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
 pub enum OutputConstraint<Obj> {
-    OfType {
-        #[serde(rename = "constraintObj")]
-        constraint_obj: Obj,
-        #[serde(rename = "type")]
-        of_type: String,
-    },
+    OfType(OfType<Obj>),
     CmpInType {
         #[serde(rename = "constraintObjs")]
         constraint_objs: (Obj, Obj),
@@ -108,15 +111,33 @@ pub enum OutputConstraint<Obj> {
 pub type VisibleGoal = OutputConstraint<InteractionPoint>;
 pub type InvisibleGoal = OutputConstraint<String>;
 
+impl<Obj> OutputConstraint<Obj> {
+    pub fn try_into_of_type(self) -> Result<OfType<Obj>, Self> {
+        if let OutputConstraint::OfType(o) = self {
+            Ok(o)
+        } else {
+            Err(self)
+        }
+    }
+}
+
 pub trait CollectObjs<Obj> {
     fn collect_objs(&self, collect: impl FnMut(&Obj) + Copy);
 }
 
-impl<Obj> CollectObjs<Obj> for JustSomething<Obj> {
-    fn collect_objs(&self, collect: impl FnMut(&Obj) + Copy) {
-        collect(&self.constraint_obj)
-    }
+macro_rules! simple_collect_objs {
+    ($class:ident) => {
+        impl<Obj> CollectObjs<Obj> for $class<Obj> {
+            fn collect_objs(&self, mut collect: impl FnMut(&Obj) + Copy) {
+                collect(&self.constraint_obj)
+            }
+        }
+    };
 }
+
+simple_collect_objs!(JustSomething);
+simple_collect_objs!(PostponedCheckArgs);
+simple_collect_objs!(OfType);
 
 impl<Obj> CollectObjs<Obj> for Vec<Obj> {
     fn collect_objs(&self, collect: impl FnMut(&Obj) + Copy) {
@@ -125,10 +146,18 @@ impl<Obj> CollectObjs<Obj> for Vec<Obj> {
 }
 
 impl<Obj> CollectObjs<Obj> for (Obj, Obj) {
-    fn collect_objs(&self, collect: impl FnMut<&Obj> + Copy) {
+    fn collect_objs(&self, mut collect: impl FnMut(&Obj) + Copy) {
         let (a, b) = self;
         collect(a);
         collect(b);
+    }
+}
+
+impl<Obj> CollectObjs<Obj> for (Vec<Obj>, Vec<Obj>) {
+    fn collect_objs(&self, collect: impl FnMut(&Obj) + Copy) {
+        let (a, b) = self;
+        a.collect_objs(collect);
+        b.collect_objs(collect);
     }
 }
 
@@ -142,17 +171,13 @@ impl<Obj> CollectObjs<Obj> for OutputConstraint<Obj> {
     fn collect_objs(&self, mut collect: impl FnMut(&Obj) + Copy) {
         use OutputConstraint::*;
         match self {
-            OfType { constraint_obj, .. } => collect(constraint_obj),
+            OfType(o) => o.collect_objs(collect),
             CmpInType {
                 constraint_objs, ..
             } => constraint_objs.collect_objs(collect),
             CmpElim {
-                constraint_objs: (xs, ys),
-                ..
-            } => {
-                xs.collect_objs(collect);
-                ys.collect_objs(collect);
-            }
+                constraint_objs, ..
+            } => constraint_objs.collect_objs(collect),
             JustType(a) => a.collect_objs(collect),
             JustSort(a) => a.collect_objs(collect),
             CmpTypes(c) => c.collect_objs(collect),
@@ -162,7 +187,7 @@ impl<Obj> CollectObjs<Obj> for OutputConstraint<Obj> {
             Guard { constraint, .. } => constraint.collect_objs(collect),
             Assign { constraint_obj, .. } => collect(constraint_obj),
             TypedAssign { constraint_obj, .. } => collect(constraint_obj),
-            PostponedCheckArgs(o) => unimplemented!(),
+            PostponedCheckArgs(o) => o.collect_objs(collect),
             IsEmptyType { .. } => {}
             SizeLtSat { .. } => {}
             FindInstanceOF { constraint_obj, .. } => collect(constraint_obj),
