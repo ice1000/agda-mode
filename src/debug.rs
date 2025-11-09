1aec2567ec4f4e3dd87e2c168e17c308a0f0246e
eldesh <nephits@gmail.com>
2025-11-09 19:54:23 +0900

    [ fix ] Avoid warning 'shared references to mutable statics'
    > shared references to mutable statics are dangerous;
    > it's undefined behavior if the static is mutated or if a mutable reference is created for it while the shared reference lives

--- src/debug.rs
+++ src/debug.rs
@@ -1,52 +1,65 @@
-static mut DEBUG_COMMAND: Option<Box<dyn Fn(String)>> = None;
-static mut DEBUG_RESPONSE: Option<Box<dyn Fn(String)>> = None;
+use std::sync::Mutex;
 
-pub unsafe fn debug_command_via(f: impl Fn(String) + 'static) {
-    DEBUG_COMMAND = Some(Box::new(f));
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref DEBUG_COMMAND: Mutex<Option<Box<dyn Fn(String) + Send>>> = Mutex::new(None);
+    static ref DEBUG_RESPONSE: Mutex<Option<Box<dyn Fn(String) + Send>>> = Mutex::new(None);
 }
 
-pub unsafe fn debug_response_via(f: impl Fn(String) + 'static) {
-    DEBUG_RESPONSE = Some(Box::new(f));
+pub unsafe fn debug_command_via(f: impl Fn(String) + Send + 'static) {
+    let mut command = DEBUG_COMMAND.lock().unwrap();
+    *command = Some(Box::new(f));
+}
+
+pub unsafe fn debug_response_via(f: impl Fn(String) + Send + 'static) {
+    let mut response = DEBUG_RESPONSE.lock().unwrap();
+    *response = Some(Box::new(f));
 }
 
 pub unsafe fn dont_debug_command() {
-    DEBUG_COMMAND = None;
+    let mut command = DEBUG_COMMAND.lock().unwrap();
+    *command = None;
 }
 
 pub unsafe fn dont_debug_response() {
-    DEBUG_RESPONSE = None;
+    let mut response = DEBUG_RESPONSE.lock().unwrap();
+    *response = None;
 }
 
 pub(crate) unsafe fn debug_command(s: String) -> bool {
-    DEBUG_COMMAND.as_ref().map(|f| f(s)).is_some()
+    let command = DEBUG_COMMAND.lock().unwrap();
+    command.as_ref().map(|f| f(s)).is_some()
 }
 
 pub(crate) unsafe fn debug_response(s: String) -> bool {
-    DEBUG_RESPONSE.as_ref().map(|f| f(s)).is_some()
+    let response = DEBUG_RESPONSE.lock().unwrap();
+    response.as_ref().map(|f| f(s)).is_some()
 }
 
 pub unsafe fn toggle_debug_command() {
-    match DEBUG_COMMAND {
+    let mut command = DEBUG_COMMAND.lock().unwrap();
+    match *command {
         None => {
             println!("Command debug mode is ON");
             debug_command_via(|s| print!("{}", s))
         }
         Some(_) => {
             println!("Command debug mode is OFF");
-            DEBUG_COMMAND = None
+            *command = None;
         }
     }
 }
 
 pub unsafe fn toggle_debug_response() {
-    match DEBUG_RESPONSE {
+    match DEBUG_RESPONSE.lock().unwrap().as_ref() {
         None => {
             println!("Response debug mode is ON");
             debug_response_via(|s| print!("{}", s))
         }
         Some(_) => {
             println!("Response debug mode is OFF");
-            DEBUG_RESPONSE = None
+            *DEBUG_RESPONSE.lock().unwrap() = None
         }
     }
 }
